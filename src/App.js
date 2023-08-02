83f1170706b474ea146e35462da2548384d0c276
jaycee808 <jamie.clark@live.co.uk>
2023-08-02 16:28:37 +0100

    updated event search items to display results from search

--- src/App.js
+++ src/App.js
@@ -8,53 +8,82 @@ const App = () => {
 
     useEffect(() => {
         // Events to display when the page loads
-        getEvents('classificationName=music')
-        .then((data) => setEvents(data))
-        .catch((error) => console.error(error));
+        getEvents('classificationName=events')
+            .then((data) => setEvents(data))
+            .catch((error) => console.error(error));
     }, []);
 
     const handleSearch = async (e) => {
         e.preventDefault();
         try {
-        const data = await getEvents(`keyword=${searchQuery}`);
-        setEvents(data);
+            const data = await getEvents(`keyword=${searchQuery}`);
+            setEvents(data);
         } catch (error) {
-        console.error(error);
+            console.error(error);
         }
     };
 
-return (
-    <div>
-        <div className="header">
-        <h1 className="logo">BOOMBOX</h1>
-        <form className="search-bar" onSubmit={handleSearch}>
-            <input
-            className="search-input"
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search for an event"
-            />
-            <button className="search-btn" type="submit">
-            Search
-            </button>
-        </form>
-        </div>
+    const handleCategoryClick = async (classificationName) => {
+        try {
+            const data = await getEvents(`classificationName=${classificationName}`);
+            setEvents(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-        <div className="event-search-categories">
-            <ul className="event-categories">
-                <li className="event-search-item">MUSIC</li>
-                <li className="event-search-item">SPORT</li>
-                <li className="event-search-item">THEATRE</li>
-                <li className="event-search-item">COMEDY</li>
-            </ul>
-        </div>
-
+    return (
         <div>
-            <EventList events={events} />
+            <div className="header">
+                <h1 className="logo">BOOMBOX</h1>
+                <form className="search-bar" onSubmit={handleSearch}>
+                    <input
+                        className="search-input"
+                        type="text"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        placeholder="Search for an event"
+                    />
+                    <button className="search-btn" type="submit">
+                        Search
+                    </button>
+                </form>
+            </div>
+
+            <div className="event-search-categories">
+                <ul className="event-categories">
+                    <li
+                        className="event-search-item"
+                        onClick={() => handleCategoryClick('music')}
+                    >
+                        MUSIC
+                    </li>
+                    <li
+                        className="event-search-item"
+                        onClick={() => handleCategoryClick('sport')}
+                    >
+                        SPORT
+                    </li>
+                    <li
+                        className="event-search-item"
+                        onClick={() => handleCategoryClick('theatre')}
+                    >
+                        THEATRE
+                    </li>
+                    <li
+                        className="event-search-item"
+                        onClick={() => handleCategoryClick('comedy')}
+                    >
+                        COMEDY
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <EventList events={events} />
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default App;
