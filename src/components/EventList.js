edef0bc8988fee3dcd09055a180e45c047099f02
jaycee808 <jamie.clark@live.co.uk>
2023-08-02 14:46:34 +0100

    updated event display styles

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -11,35 +11,37 @@ const EventList = ({ events }) => {
         return foundIndex === index;
     });
 
-    return (
-        <div className="event-list-container">
-            <h2 className="event-list-title">Events List</h2>
-            <ul className="event-list">
-                {filteredEvents.map((event) => (
-                    <li key={event.id} className="event-item">
-                        {event.images && event.images.length > 0 && (
-                            <img className="event-image" src={event.images[0].url} alt={event.name} />
-                        )}
-                        <div className="event-details">
-                            <h3 className="event-name">{event.name}</h3>
-                            {event._embedded?.venues?.length > 0 ? (
-                                <div>
-                                    <p className="event-venue">Venue: {event._embedded.venues[0].name}</p>
-                                    <EventMap event={event} />
-                                </div>
-                            ) : (
-                                <p className="event-venue">Venue information not available</p>
-                            )}
-                            <p className="event-date">Date: {event.dates.start.localDate}</p>
-                            <p className="event-time">Time: {event.dates.start.localTime}</p>
-                            <p className="event-url">
-                                URL: <a href={event.url} target="_blank" rel="noopener noreferrer">{event.url}</a>
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+return (
+    <div className="event-list-container">
+        <h2 className="event-list-title">Events List</h2>
+        <ul className="event-list">
+        {filteredEvents.map((event) => (
+            <li key={event.id} className="event-item">
+            <div className="event-item-content">
+                {event.images && event.images.length > 0 && (
+                <img className="event-image" src={event.images[0].url} alt={event.name} />
+                )}
+                <div className="event-details">
+                <h3 className="event-name">{event.name}</h3>
+                <p className="event-date">Date: {event.dates.start.localDate}</p>
+                <p className="event-time">Time: {event.dates.start.localTime}</p>
+                <p className="event-url">
+                    URL: <a href={event.url} target="_blank" rel="noopener noreferrer">{event.url}</a>
+                </p>
+                {event._embedded?.venues?.length > 0 ? (
+                    <div>
+                    <p className="event-venue">Venue: {event._embedded.venues[0].name}</p>
+                    <EventMap event={event} />
+                    </div>
+                ) : (
+                    <p className="event-venue">Venue information not available</p>
+                )}
+                </div>
+            </div>
+            </li>
+        ))}
+        </ul>
+    </div>
     );
 };
 
