1ca9745aa31dd7ef0ea06f68a27e70da9ba540c3
jaycee808 <jamie.clark@live.co.uk>
2023-08-19 16:30:34 +0100

    updated event details display and added responsive sizing

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import EventMap from './EventMap';
 
 const EventList = ({ events }) => {
-    // Filter events to remove duplicates with the same date and venue
     const filteredEvents = events.filter((event, index, self) => {
         const foundIndex = self.findIndex(
             (e) => e.dates.start.localDate === event.dates.start.localDate && 
@@ -16,30 +15,28 @@ const EventList = ({ events }) => {
             <div className="event-list">
                 {filteredEvents.map((event) => (
                     <div key={event.id} className="event-item">
+                    <img className="event-image" src={event.images[0].url} alt={event.name} />
                         <div className="event-item-content">
-                            {event.images && event.images.length > 0 && (
-                                <img className="event-image" src={event.images[0].url} alt={event.name} />
-                            )}
+                            <h3 className="event-name">{event.name}</h3>
                             <div className="event-details">
-                                <h3 className="event-name">{event.name}</h3>
                                 <p className="event-date">
                                     {new Date(event.dates.start.localDate).getDate()}{' '}
                                     {new Date(event.dates.start.localDate).toLocaleString('default', { month: 'long' })}{' '}
                                     {new Date(event.dates.start.localDate).getFullYear()}
                                 </p>
-                                <button className="ticket-btn">
-                                    <a href={event.url} target="_blank" rel="noopener noreferrer">
-                                        Find Tickets
-                                    </a>
-                                </button>
                                 {event._embedded?.venues?.length > 0 ? (
-                                    <div>
-                                        <p className="event-venue">Venue: {event._embedded.venues[0].name}</p>
+                                    <div className="event-venue">
+                                        <p>Venue: {event._embedded.venues[0].name}</p>
                                         <EventMap event={event} />
                                     </div>
                                 ) : (
                                     <p className="event-venue">Venue information not available</p>
                                 )}
+                                <button className="ticket-btn">
+                                    <a href={event.url} target="_blank" rel="noopener noreferrer">
+                                        Find Tickets
+                                    </a>
+                                </button>
                             </div>
                         </div>
                     </div>
